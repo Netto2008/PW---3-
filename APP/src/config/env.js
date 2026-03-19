75c1453c3646414ae5c717f4d0f22223179ffd2d
Ismael Ribeiro de Morais Netto <nettoetec@gmail.com>
2026-03-19 10:49:02 +0000

    Fim

--- APP/src/config/env.js
+++ APP/src/config/env.js
@@ -1,5 +1,5 @@
-const dominio = "http://localhost";
-const port = "3000";
-const baseUrl = `${dominio}:${port}`;
+// const dominio = 'https:/localhost';
+// const port = "3000";
+const baseUrl = "https://sturdy-fortnight-6956gj4gxjg7fw7w-3000.app.github.dev/";
 
 export { baseUrl };
